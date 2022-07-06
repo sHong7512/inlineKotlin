8c510428f93e5553c6466dbc1a606b19b8b29061
SoonHong Kwon <ksh7512@uangel.com>
2022-07-06 13:46:29 +0900

    Basic Example

---
+++ app/src/main/java/com/shong/inlinekotlin/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.shong.inlinekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val exText1 = findViewById<TextView>(R.id.exText1)
+        val exText2 = findViewById<TextView>(R.id.exText2)
+        val exText3 = findViewById<TextView>(R.id.exText3)
+
+        exText1.countClickListener_1({ 0 }) { cnt, str ->
+            exText1.text = "<<$str>>\nCount : $cnt"
+        }
+        exText2.countClickListener_2({ 0 }) { cnt, str ->
+            exText2.text = "<<$str>>\nCount : $cnt"
+        }
+        exText3.countClickListener_3({ 0 }) {tv, cnt ->
+            tv.text = "<<example 3 reified Extension>>\nCount : $cnt"
+        }
+    }
+
+    private fun View.countClickListener_1(initialCount: () -> Int, clickOperator: (String, Int) -> Unit){
+        var cnt = initialCount()
+        setOnClickListener {
+            cnt++
+            clickOperator("example 1 basic Extension", cnt)
+        }
+    }
+
+    private inline fun View.countClickListener_2(initialCount: () -> Int, crossinline clickOperator: (String, Int) -> Unit){
+        var cnt = initialCount()
+        setOnClickListener {
+            cnt++
+            clickOperator("example 2 Inline, crossinline Extension", cnt)
+        }
+    }
+
+    private inline fun <reified T: View> T.countClickListener_3(initialCount: () -> Int, crossinline clickOperator: (T, Int) -> Unit){
+        var cnt = initialCount()
+        setOnClickListener { view ->
+            cnt++
+            clickOperator(view as T, cnt)
+        }
+    }
+}
